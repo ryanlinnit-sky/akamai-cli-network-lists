9ee32a52a388ed83487e5fc1e7bbaf4d32f0cb60
Ryan Linnit <ryan.linnit@sky.uk>
2024-08-22 17:29:17 +0100

    structure

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Development
 
-This project is using [hatch](https://hatch.pypa.io/latest/) as it's build system. For this reason I also used it to manage the development environment. The following commands can be used or alternatively you're free to use the Python `venv` module.
+This project is using [hatch](https://hatch.pypa.io/latest/) as it's build system. For this reason I also used it to manage the development environment. The following commands can be used to create and activate a virtual environment. Alternatively you're free to use the Python `venv` module.
 
 You may need to [install](https://hatch.pypa.io/latest/install/) `hatch` (`pip install hatch`)
 
@@ -12,6 +12,12 @@ hatch env create
 hatch shell
 ```
 
+
+The main "binary" is located in `bin/akamai-network-lists.py`. The reason is because the akamai-cli tool has it's own format a python module must adhere to. It [looks for files](https://github.com/akamai/cli/blob/20c6521bfe3cb129bc5a41c81e206bba27e40efd/pkg/commands/command.go#L372) under the `bin` directory that [match the pattern](https://github.com/akamai/cli/blob/20c6521bfe3cb129bc5a41c81e206bba27e40efd/pkg/commands/command.go#L286) of `akamai-command-name`.
+
+We also [need to list our dependencies](https://github.com/akamai/cli/blob/20c6521bfe3cb129bc5a41c81e206bba27e40efd/pkg/packages/python.go#L128) in a `requirements.txt` file, as we are using `pyproject.toml` to define our build system requirements and dependencies, this means we can solely include `.` in `requirements.txt` and this module will be installed.
+
+There is some information in the [README.md](https://github.com/akamai/cli/blob/20c6521bfe3cb129bc5a41c81e206bba27e40efd/README.md#command-package-metadata) for akamai-cli, but a lot of the specifics seem un-written..
 
 ## Build
 
@@ -27,9 +33,13 @@ python3 -m twine upload dist/*
 ## Install in akamai-cli
 
 ```bash
+# i'm using ubuntu 22.04 and there's an issue with the default python pip package
+# for that reason i need to use a later version of python that the akamai-cli
+# can use, the following three lines may not be necessary on any other configuration
 python3.11 -m venv test_venv
 source test_venv/bin/activate
 PYTHONUSERBASE="test_venv/lib/python3.11/site-packages"
+
 akamai install https://github.com/ryanlinnit-sky/akamai-cli-network-lists.git
 
 akamai network-lists
--- bin/akamai-network-lists.py
+++ bin/akamai-network-lists.py
@@ -1,3 +1,109 @@
-#!/bin/env python
+"""
+Some of the initial code for this project was borrowed from the Akamai cli-cloudlets project
+https://github.com/akamai/cli-cloudlets/blob/56825e078b6014ce2f3b8af86fdcebf21d9e08d7/bin/akamai-cloudlets.py
 
-print("This is a bin")
+"""
+import click
+import configparser
+import importlib.metadata
+import logging
+import os
+import requests
+import sys
+from akamai.edgegrid import EdgeGridAuth
+from akamai.edgegrid import EdgeRc
+from akamai_network_lists.network_lists_api_wrapper import NetworkLists
+
+
+logger = logging.getLogger()
+
+# file_handler = logging.FileHandler('akamai_network_lists.log')
+# file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# file_handler.setFormatter(file_formatter)
+# logger.addHandler(file_handler)
+
+console_formatter = logging.Formatter('%(message)s')
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(console_formatter)
+logger.addHandler(console_handler)
+logger.setLevel(logging.INFO)
+
+
+def init_config(edgerc_file, section):
+    if not edgerc_file:
+        if not os.getenv('AKAMAI_EDGERC'):
+            edgerc_file = os.path.join(os.path.expanduser('~'), '.edgerc')
+        else:
+            edgerc_file = os.getenv('AKAMAI_EDGERC')
+
+    if not os.access(edgerc_file, os.R_OK):
+        logger.error(f'ERROR: Unable to read edgerc file {edgerc_file}')
+        exit(1)
+
+    if not section:
+        if not os.getenv('AKAMAI_EDGERC_SECTION'):
+            section = 'network-lists'
+        else:
+            section = os.getenv('AKAMAI_EDGERC_SECTION')
+
+    try:
+        edgerc = EdgeRc(edgerc_file)
+        base_url = edgerc.get(section, 'host')
+        session = requests.Session()
+        session.auth = EdgeGridAuth.from_edgerc(edgerc, section)
+    except configparser.NoSectionError:
+        logger.error(f'ERROR: edgerc section {section} not found')
+        exit(1)
+    except Exception:
+        logger.error(f'ERROR: Unknown error occurred trying to read edgerc file ({edgerc_file})')
+        exit(1)
+    return base_url, session
+
+
+class Config:
+    def __init__(self):
+        pass
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
+
+@click.group(context_settings={'help_option_names': ['-h', '--help']})
+@click.option('-e', '--edgerc', metavar='', default=os.path.join(os.path.expanduser('~'), '.edgerc'), help='Location of the credentials file [$AKAMAI_EDGERC]', required=False)
+@click.option('-s', '--section', metavar='', help='Section of the credentials file [$AKAMAI_EDGERC_SECTION]', required=False)
+@click.option('-a', '--account-key', metavar='', help='Account Key', required=False)
+@click.version_option(version=importlib.metadata.version("akamai_network_lists"))
+@pass_config
+def cli(config, edgerc, section, account_key):
+    f'''
+    Akamai CLI for Network Lists {importlib.metadata.version("akamai_network_lists")}
+    '''
+    config.edgerc = edgerc
+    config.section = section
+    config.account_key = account_key
+
+
+@cli.command()
+@click.pass_context
+def help(ctx):
+    '''
+    Show help information
+    '''
+    print(ctx.parent.get_help())
+
+@cli.command()
+@pass_config
+def list(ctx):
+    print("this is a test command.")
+
+
+def main():
+    try:
+        cli_status = cli(prog_name="akamai_network_lists", obj={})
+    except Exception as e:
+        logger.error(e)
+        exit(1)
+
+if __name__ == "__main__":
+    main()
